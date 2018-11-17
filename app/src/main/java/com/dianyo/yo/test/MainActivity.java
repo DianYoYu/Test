96ab98f41799e13486784e5cdcf5e0ee56e40984
1500380a <wzzeng@softmobile.com.tw>
2018-11-18 00:09:00 +0800

    test3

--- app/src/main/java/com/dianyo/yo/test/MainActivity.java
+++ app/src/main/java/com/dianyo/yo/test/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("test", "test");
         Log.d("test2", "test2");
+        Log.d("test3", "test3");
     }
 }
